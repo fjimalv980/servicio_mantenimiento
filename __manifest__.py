463e5040805be7e8f0d3f21c56f1494fb71f019f
Francisco <fjimalv980@g.educaand.es>
2025-02-10 12:00:04 +0100

    Cambios en el manifest

--- __manifest__.py
+++ __manifest__.py
@@ -5,12 +5,12 @@
     'author': 'Francisco Jiménez',
     'category': 'Custom',
     'summary': 'Gestión de Mantenimiento de Productos',
-    'depends': ['base', 'garantia.producto'],
+    'depends': ['base', 'garantias'],
     'data': [
         'security/ir.model.access.csv',
         'views/servicio_mantenimiento_views.xml',
     ],
-    'icon': '/garantias/static/description/icon55.png',
+    'icon': '/security/static/description/icon55.png',
     'installable': True,
     'application': True,
 }
